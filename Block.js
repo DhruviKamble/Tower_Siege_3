e03e750568a9e2a671ec20a39ee5c7e0f1d9b5dc
DhruviKamble <70420876+DhruviKamble@users.noreply.github.com>
2021-01-30 17:38:19 -0700

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,29 @@
+class Block extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+        this.Visibility = 255;
+      
+    }
+
+    display() {
+      if (this.body.speed < 2.5) {
+        super.display();
+      }
+      else{
+        World.remove(world,this.body);
+        push();
+        this.Visibility = this.Visibility - 2;
+        tint(255,this.Visibility);
+        pop();
+        
+      }
+    }
+
+    score() {
+      if(this.Visibility<0 && this.Visibility>-10) {
+          score = score+10;
+      }
+    }
+   
+  };
+  
